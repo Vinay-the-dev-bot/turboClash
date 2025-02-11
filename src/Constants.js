5adac4e0af10ffdfad51930074b1ad542300659e
Vinay Meti <vinaygouda.meti16@gmail.com>
2025-02-11 13:14:47 +0530

    constants

---
+++ src/Constants.js
@@ -0,0 +1,5 @@
+export const baseURL = "http://localhost:3000";
+export const cars = [
+  { name: "Lambo", stats: { health: 50, attack: 10, defense: 5 } },
+  { name: "Ferrari", stats: { health: 100, attack: 5, defense: 10 } }
+];
